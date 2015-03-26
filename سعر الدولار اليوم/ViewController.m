4dcbfdb1c2bbdacd7a03719390e1d6258e06d243
adhamgad <adhamgad1@gmail.com>
2015-03-26 18:01:16 +0200

    initial commit

--- سعر الدولار اليوم/ViewController.m
+++ سعر الدولار اليوم/ViewController.m
@@ -17,11 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"TestRep");
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
